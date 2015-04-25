fcad3565e20560ac89f1868e13c3e6246063fad6
MattDSquared <matthew.d.davis@gmail.com>
2015-04-24 19:19:20 -0700

    renamed question1 to plot1, deleted old file

--- question1_total_emissions_trend.R
+++
@@ -1,6 +0,0 @@
-# =============================================================================
-# Question 1: Have total emissions from PM2.5 decreased in the United States 
-# from 1999 to 2008? Using the base plotting system, make a plot showing the 
-# total PM2.5 emission from all sources for each of the years 1999, 2002, 2005,
-# and 2008.
-# =============================================================================
